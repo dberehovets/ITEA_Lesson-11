7dbe0a2917310f2cc94b1a29ac848a327db9b1fa
Dmytro <58703014+dberehovets@users.noreply.github.com>
2020-02-03 14:02:05 +0200

    updated

--- home/main.py
+++ home/main.py
@@ -1,43 +1,40 @@
-from mongoengine.errors import  ValidationError
+from mongoengine.errors import  ValidationError, DoesNotExist
 from telebot import TeleBot
 from config import TOKEN
 from home.model import User
 
 bot = TeleBot(TOKEN)
 
-user_fields = []
-user_data = dict()
-
 
 @bot.message_handler(commands=["start"])
 def start_bot(message):
-    global user_fields
-    user_fields = User.get_fields()
-    bot.send_message(message.chat.id, "Hello! Please type your name.")
+    try:
+        _get_data(message)
+    except DoesNotExist:
+        User(telegram_id=message.from_user.id).save()
+        bot.send_message(message.chat.id, "Hello! Please type your name.")
 
 
 @bot.message_handler(content_types=["text"])
 def get_name(message):
-    try:
-        field = user_fields.pop(0)
-        user_data[field] = message.text
-        if user_fields:
-            bot.send_message(message.chat.id, f"Please type your {user_fields[0]}")
-        else:
+    user = User.objects(telegram_id=message.from_user.id).get()
+    for key, value in user.get_fields().items():
+        if not value:
+            users = User._get_collection()
+            users.find_one_and_update({"telegram_id": message.from_user.id}, {"$set": {key: message.text}})
+            break
+    _get_data(message)
 
-            if User.objects(email=user_data["email"]).count() != 0:
-                users = User._get_collection()
-                users.find_one_and_update({"email": user_data["email"]}, {"$set": user_data})
-                user_data.clear()
-            else:
-                try:
-                    User(**user_data).save()
-                    user_data.clear()
-                    bot.send_message(message.chat.id, "Data is stored. Thank you!")
-                except ValidationError as err:
-                    bot.send_message(message.chat.id, f"Data was not stored! {err}")
 
-    except IndexError:
+def _get_data(message):
+    user = User.objects(telegram_id=message.from_user.id).get()
+    all_fields_filled = True
+    for key, value in user.get_fields().items():
+        if not value:
+            all_fields_filled = False
+            bot.send_message(message.chat.id, f"Please type your {key}")
+            break
+    if all_fields_filled:
         bot.send_message(message.chat.id, "Data is stored. Thank you!")
 
 
--- home/model.py
+++ home/model.py
@@ -3,13 +3,19 @@ connect("telegram_users")
 
 
 class User(Document):
-    name = StringField(required=True, max_length=255)
-    surname = StringField(required=True, max_length=255)
-    phone_number = StringField(max_length=20)
-    email = EmailField()
-    address = StringField(max_length=255)
-    wishes = StringField(max_length=4096)
+    name = StringField(default=None, max_length=255)
+    surname = StringField(default=None, max_length=255)
+    phone_number = StringField(default=None, max_length=20)
+    email = EmailField(default=None)
+    address = StringField(default=None, max_length=255)
+    wishes = StringField(default=None, max_length=4096)
+    telegram_id = IntField()
 
-    @classmethod
-    def get_fields(cls):
-        return ["name", "surname", "phone_number", "email", "address", "wishes"]
+    def get_fields(self):
+        return {"name": self.name,
+                "surname": self.surname,
+                "phone_number": self.phone_number,
+                "email": self.email,
+                "address": self.address,
+                "wishes": self.wishes
+                }
